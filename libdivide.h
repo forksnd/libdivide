ea0fb1ad79a116cd561595c930bcaf33147b3e6c
kimwalisch <4656219+kimwalisch@users.noreply.github.com>
2026-09-06 09:28:51 +0000

    Auto increment version to 5.4.0

--- libdivide.h
+++ libdivide.h
@@ -12,9 +12,9 @@
 #define LIBDIVIDE_H
 
 // *** Version numbers are auto generated - do not edit ***
-#define LIBDIVIDE_VERSION "5.3.0"
+#define LIBDIVIDE_VERSION "5.4.0"
 #define LIBDIVIDE_VERSION_MAJOR 5
-#define LIBDIVIDE_VERSION_MINOR 3
+#define LIBDIVIDE_VERSION_MINOR 4
 #define LIBDIVIDE_VERSION_PATCH 0
 
 #include <stdint.h>
